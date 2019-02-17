160e2f3dd46c5ada84c92b13cb05e8d7dcd10442
Regaip KURT <regkrt@gmail.com>
2019-02-17 05:03:31 +0300

    Update Realtime_Graph_PYPlot.py

--- Realtime_Graph_PYPlot.py
+++ Realtime_Graph_PYPlot.py
@@ -1,7 +1,7 @@
 import random
 import matplotlib.pyplot as plt
 import time
-sozluk = {"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0}
+sozluk = {"0":0,"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0}
 i=0
 while i <= 1000:
     x = str(random.randint(0,9))
